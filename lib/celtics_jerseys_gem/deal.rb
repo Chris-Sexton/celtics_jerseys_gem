52872eee58e01ab97d5fb5af763f2ec013a246dd
Chris-Sexton <champlain2013@gmail.com>
2016-10-17 15:19:44 -0400

    Gem now lists 5 different Celtics jerseys and their prices.

--- lib/celtics_jerseys_gem/deal.rb
+++ lib/celtics_jerseys_gem/deal.rb
@@ -8,16 +8,62 @@ class CelticsJerseysGem::Deal
   def self.scrape_deals
     deals = []
     
-    deals << self.scrape_celtics
+    deals << self.scrape_horford
+    deals << self.scrape_thomas
+    deals << self.scrape_brown
+    deals << self.scrape_smart
+    deals << self.scrape_crowder
     deals
   end
 
-  def self.scrape_celtics
-    doc = Nokogiri::HTML(open("http://www.celticsstore.com/mens/index.php?v=celtics_jerseys_mens"))
-    name = doc.search("div.prodName").text
-    price = doc.search("span.originalPrice").text
-    url = doc.search("a.productLink")
+  def self.scrape_horford
+    doc = Nokogiri::HTML(open("http://www.celticsstore.com/adidas-celtics-al-horford-replica-road-jersey/detail.php?p=1023356&v=celtics_jerseys_mens"))
     
-    binding.pry
+    deal = self.new
+    deal.name = doc.search("h1.prodInfoTitle").text.strip
+    deal.price = doc.search("span.yourPrice").text.strip
+    deal.url = doc.search("a.productLink")
+
+    deal
+  end
+
+  def self.scrape_thomas
+    doc = Nokogiri::HTML(open("http://www.celticsstore.com/adidas-celtics-isaiah-thomas-replica-road-jersey/detail.php?p=822950&v=celtics_jerseys_mens"))
+    deal = self.new
+    deal.name = doc.search("h1.prodInfoTitle").text.strip
+    deal.price = doc.search("span.yourPrice").text.strip
+    deal.url = doc.search("a.productLink")
+
+    deal
+  end
+
+  def self.scrape_brown
+    doc = Nokogiri::HTML(open("http://www.celticsstore.com/adidas-celtics-jaylen-brown-replica-home-jersey/detail.php?p=1023336&v=celtics_jerseys_mens"))
+    deal = self.new
+    deal.name = doc.search("h1.prodInfoTitle").text.strip
+    deal.price = doc.search("span.yourPrice").text.strip
+    deal.url = doc.search("a.productLink")
+
+    deal
+  end
+
+  def self.scrape_smart
+    doc = Nokogiri::HTML(open("http://www.celticsstore.com/adidas-celtics-marcus-smart-revolution-30-replica-home-jersey/detail.php?p=604294&v=celtics_jerseys_mens"))
+    deal = self.new
+    deal.name = doc.search("h1.prodInfoTitle").text.strip
+    deal.price = doc.search("span.yourPrice").text.strip
+    deal.url = doc.search("a.productLink")
+
+    deal
+  end
+
+  def self.scrape_crowder
+    doc = Nokogiri::HTML(open("http://www.celticsstore.com/adidas-celtics-jae-crowder-swingman-20-road-jersey/detail.php?p=821692&v=celtics_jerseys_mens"))
+    deal = self.new
+    deal.name = doc.search("h1.prodInfoTitle").text.strip
+    deal.price = doc.search("span.yourPrice").text.strip
+    deal.url = doc.search("a.productLink")
+
+    deal
   end
 end
